32041e4631ae1e73dac90063b80286e56f3cfe23
uangbesok <45453585+uangbesok@users.noreply.github.com>
2020-03-26 16:34:07 +0800

    Add redirect to sign in if user already passed autentication

--- src/App.js
+++ src/App.js
@@ -1,71 +1,74 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
-import { connect } from 'react-redux';
-import { setCurrentUserAction } from './redux/user/user.actions'
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { setCurrentUserAction } from "./redux/user/user.actions";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInSignOutPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
-import { auth, createUserProfileDocument } from './firebase/firebase.utils'
-
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
-
   unsubscribeFromAuth = null;
 
-  componentDidMount()
-  {
+  componentDidMount() {
     const { setCurrentUserAction } = this.props;
 
-    auth.onAuthStateChanged(async userAuth => 
-      {
-        if(userAuth)
-        {
-          //Create user in firestore users collection and return user reference
-          const userRef = await createUserProfileDocument(userAuth);
+    auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        //Create user in firestore users collection and return user reference
+        const userRef = await createUserProfileDocument(userAuth);
 
-          //Update state when user document changes
-          userRef.onSnapshot(snapShot => {
-            setCurrentUserAction({
-                id: snapShot.id,
-                ...snapShot.data(),  
-            });
-          })
-        }
-        else setCurrentUserAction(userAuth);
-      });
-      
-    }
+        //Update state when user document changes
+        userRef.onSnapshot(snapShot => {
+          setCurrentUserAction({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
+      } else setCurrentUserAction(userAuth);
+    });
+  }
 
-  componentWillUnmount()
-  {
+  componentWillUnmount() {
     //Stop listening to auth events. Needed to avoid memory leaks.
     this.unsubscribeFromAuth();
   }
 
-  render()
-  {
-    console.log('state render ' , this.state);
+  render() {
     return (
       <>
         <Header />
         <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/shop" component={ShopPage} />
-            <Route exact path="/signin" component={SignInSignOutPage} />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact 
+                 path="/signin"
+                 render={() => this.props.currentUser ?
+                               <Redirect to="/" /> :
+                               <SignInSignOutPage />
+                               } />
         </Switch>
       </>
     );
   }
-  
 }
 
-const mapDispatchToProps = dispatch => (
-  {
-    setCurrentUserAction: user => dispatch(setCurrentUserAction(user))
-  }
-);
+// const mapDispatchToProps = dispatch => (
+//   {
+//     setCurrentUserAction: user => dispatch(setCurrentUserAction(user))
+//   }
+// );
 
-export default connect(null, mapDispatchToProps)(App);
+// Short form of mapDispatchToProps. To check if it works.
+const mapDispatchToProps = {
+  setCurrentUserAction
+};
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
