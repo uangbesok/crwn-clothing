f086f69b5a2777b47bc1437c7e30711eb7391550
uangbesok <45453585+uangbesok@users.noreply.github.com>
2020-03-28 22:24:15 +0800

    Update cart icon component with total count from redux state. Add cart items and total items count memoization selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,17 +4,24 @@ import './cart-icon.styles.scss'
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { totalItemCount } from "../../redux/cart/cart.selectors"
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, totalItemCount }) => (
     <div className='cart-icon' onClick={ toggleCartHidden }>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{totalItemCount}</span>
     </div>
 );
 
 const mapDispatchToProps = {
     toggleCartHidden
   };
+
+//To reconsider whether it's better to recalculate new total quantity in the moment
+// when new item added to cart
+const mapStateToProps = (state) => ({
+    totalItemCount: totalItemCount(state),
+})
   
   
-  export default connect(null, mapDispatchToProps)(CartIcon);
+  export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
